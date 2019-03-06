1ee63b1b140c6de9afba543e9b0a6794e51738ae
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2019-03-05 21:13:01 -0800

    Clean up 🙃

--- src/App.js
+++ src/App.js
@@ -5,76 +5,86 @@ import moment from 'moment';
 import nanoId from 'nano-id';
 
 // date, time, slidervalue, notes, id
-let database = []
+let database = [];
 
 class App extends Component {
   state = {
     sliderValue: 0,
-    notes: "",
-    view: "form", //overview
-  
-  }
+    notes: '',
+    view: 'form' // 'form', overview'
+  };
 
-  handleSlide = (event) => {
+  handleSlide = ({ target }) => {
     this.setState({
-      sliderValue: event.target.value,
-    })
-  }
+      sliderValue: target.value
+    });
+  };
 
-  handleChange = (event) => {
+  handleChange = ({ target }) => {
     this.setState({
-      notes: event.target.value,
-    })
-  }
-  
+      notes: target.value
+    });
+  };
+
   handleSubmit = () => {
-    const {sliderValue, notes} = this.state
+    const { sliderValue, notes } = this.state;
 
-    database.push(
-      {
-      sliderValue: sliderValue,
-      notes: notes,
+    database.push({
+      sliderValue,
+      notes,
       date: new Date().toLocaleDateString(),
       time: moment().format('LT'),
-      id: nanoId(10),
-      })
-    
+      id: nanoId(10)
+    });
+
     this.setState({
-      view: "overview",
-    })
-  }
+      view: 'overview'
+    });
+  };
 
   render() {
-    const { sliderValue, notes, view } = this.state
-    console.log(database)
+    const { sliderValue, notes, view } = this.state;
+    console.log(database);
     return (
       <div className="App">
-        {view === "form" ?
-        (<Fragment>
-          <h2>Hello, how are you feeling?</h2>
-          <p>Between 1 to 10, <br/>with 10 being great and 1 being moderately low </p>
-          <Slider sliderValue= {sliderValue} notes= {notes} handleSlide={this.handleSlide} handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
-        </Fragment>
-        ):
-        (
+        {view === 'form' ? (
           <Fragment>
-          { database.map(data=>(
-              <div className= "overview" key={data.id}>
+            <h2>Hello, how are you feeling?</h2>
+            <p>
+              Between 1 to 10, <br />
+              with 10 being great and 1 being moderately low{' '}
+            </p>
+            <Slider
+              sliderValue={sliderValue}
+              notes={notes}
+              handleSlide={this.handleSlide}
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+            />
+          </Fragment>
+        ) : (
+          <Fragment>
+            {database.map(data => (
+              <div className="overview" key={data.id}>
                 <h2>Here is your overview:</h2>
                 <p>You rated your mood as: {data.sliderValue} </p>
-                {data.sliderValue <5 ? (<p><em>you aren't feeling so great</em></p>): (<p>Always try to take one day at a time </p>)}
+                {data.sliderValue < 5 ? (
+                  <p>
+                    <em>you aren't feeling so great</em>
+                  </p>
+                ) : (
+                  <p>Always try to take one day at a time </p>
+                )}
                 <p>Notes: {data.notes}</p>
                 <p>Date: {data.date}</p>
                 <p>Time: {data.time}</p>
               </div>
-            ))
-          }
+            ))}
           </Fragment>
-        )
-      }
-      <footer>
-        <p>made with ♥ by hannah & cristien.</p>
-      </footer>
+        )}
+        <footer>
+          <p>made with ♥ by hannah & cristien.</p>
+        </footer>
       </div>
     );
   }
